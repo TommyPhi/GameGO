952cc2b12633af20c0c52b98626e304ad830c945
TommyPhi <tommy.phi04@gmail.com>
2023-10-27 14:29:23 -0400

    added footer content and side navbar

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "350px";
+    document.getElementById("sidenav").style.padding = "60px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("sidenav").style.padding = "0";
+    document.body.style.backgroundColor = "white";
+  }
